cd093b73780276206aea902cc6ee2a1f5ec00f87
grymyk <mykola.grytsai>
2017-01-13 18:38:51 +0200

    autocall of gallery.js

--- app/scripts/gallery.js
+++ app/scripts/gallery.js
@@ -1,47 +1,49 @@
-'use strict';
+(function ($) {
+    'use strict';
 
-let template;
+    let template;
 
-let serverUrl = 'https://expigrt.herokuapp.com';
+    let serverUrl = 'https://expigrt.herokuapp.com';
 
 //let socket = io.connect('http://localhost:5000');
 
-console.log('--- io --- \n', io.connect.toString() );
+    console.log('--- io --- \n', io.connect.toString() );
 
-let socket = io.connect(serverUrl);
+    let socket = io.connect(serverUrl);
 
-let templateUrl = serverUrl + '/templates/item.hbs';
+    let templateUrl = serverUrl + '/templates/item.hbs';
 
-$.get(templateUrl, (data) => {
-    template = Handlebars.compile(data);
-}, 'html')
-.done( () => {
-    console.log('second success');
-})
-.fail( () => {
-    console.log('error');
-})
-.always( () => {
-    console.log('finished');
-});
+    $.get(templateUrl, (data) => {
+        template = Handlebars.compile(data);
+    }, 'html')
+        .done( () => {
+            console.log('second success');
+        })
+        .fail( () => {
+            console.log('error');
+        })
+        .always( () => {
+            console.log('finished');
+        });
 
-Handlebars.registerHelper('human_time', (timestamp) => {
-    return moment.unix(timestamp).fromNow();
-});
+    Handlebars.registerHelper('human_time', (timestamp) => {
+        return moment.unix(timestamp).fromNow();
+    });
 
-socket.on('fotos', (data) => {
-    console.log(data);
+    socket.on('fotos', (data) => {
+        console.log(data);
 
-    let galleryHolder = $('#gallery').find('.gallery');
+        let galleryHolder = $('#gallery').find('.gallery');
 
-    let html = template(data);
+        let html = template(data);
 
-    $(html).hide();
+        $(html).hide();
 
-    galleryHolder
-        .empty()
-        .append( $(html) )
-        .fadeIn('slow');
+        galleryHolder
+            .empty()
+            .append( $(html) )
+            .fadeIn('slow');
 
-    //$(html).hide().insertBefore(galleryHolder).fadeIn('slow');
-});
+        //$(html).hide().insertBefore(galleryHolder).fadeIn('slow');
+    });
+})(jQuery);
