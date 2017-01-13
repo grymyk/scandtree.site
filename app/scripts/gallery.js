dbe82c8813ca1464b1ce9b38333deea73641df43
grymyk <mykola.grytsai>
2017-01-13 17:38:48 +0200

    add gallery.js

--- app/scripts/gallery.js
+++ app/scripts/gallery.js
@@ -2,12 +2,25 @@
 
 let template;
 
-//let socket = io.connect('http://localhost:5000');
-let socket = io.connect('https://expigrt.herokuapp.com');
+let serverUrl = 'https://expigrt.herokuapp.com';
 
-$.get('/templates/item.hbs', (data) => {
+//let socket = io.connect('http://localhost:5000');
+let socket = io.connect(serverUrl);
+
+let templateUrl = serverUrl + '/templates/item.hbs';
+
+$.get(templateUrl, (data) => {
     template = Handlebars.compile(data);
-}, 'html');
+}, 'html')
+.done( () => {
+    console.log("second success");
+})
+.fail( () => {
+    console.log("error");
+})
+.always( () => {
+    console.log("finished");
+});
 
 Handlebars.registerHelper('human_time', (timestamp) => {
     return moment.unix(timestamp).fromNow();
